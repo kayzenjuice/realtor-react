9eb082d81125a310b8e359a40309ed102e8f663a
kayzenjuice <78895146+kayzenjuice@users.noreply.github.com>
2022-12-28 17:44:58 +1100

    Final commit added rent and sale pages

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import EditListing from "./pages/EditListing";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Listing from "./pages/Listing";
+import Category from "./pages/Category";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
           <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/category/:categoryName" element={<Category />}></Route>
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
